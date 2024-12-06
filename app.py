69bb2f1455fabd10820856102dda5e5a25e10690
Lukáš Vašátko <lukas.vasatko@seznam.cz>
2024-12-06 10:38:21 +0100

    registrace (skoro hotovo - chyba alerty v mainstructure)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask import Flask, redirect, url_for, render_template, request, session, flash, get_flashed_messages
 import sqlite3
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -6,7 +6,7 @@ import logging
 
 
 app = Flask(__name__)
-
+app.secret_key = "a7823468234732n234489cb233257823"
 
 logging.basicConfig(level=logging.DEBUG)
 app.logger.debug('Flash volán')
@@ -23,11 +23,11 @@ def get_db_connection():
 
 @app.route('/')
 def home1():
-    return redirect(url_for('domu'))
+    return redirect(url_for('domu'), msgs=get_flashed_messages(with_categories=True))
 
 @app.route('/domu')
 def domu():
-    return render_template('index.html')
+    return render_template('index.html', msgs=get_flashed_messages(with_categories=True))
 
 @app.route('/registrace', methods=['GET', 'POST'])
 def registrace():
@@ -42,7 +42,7 @@ def registrace():
 
         if heslo != heslo_znova:
             flash('Hesla se neshodují. Zkuste to znovu.', 'error')
-            return render_template('registrace.html')
+            return render_template('registrace.html', msgs=get_flashed_messages(with_categories=True))
 
         hashed_password = generate_password_hash(heslo)
 
@@ -54,25 +54,26 @@ def registrace():
             """, (jmeno, prijmeni, email, telefon, hashed_password, role))
             conn.commit()
             flash('Registrace byla úspěšná! Můžete se přihlásit.', 'success')
-            return redirect(url_for('prihlaseni'))
+            return render_template('registrace.html', msgs=get_flashed_messages(with_categories=True))
         except sqlite3.IntegrityError:
             flash('E-mail je již registrován. Zkuste jiný e-mail.', 'error')
+            return render_template('registrace.html', msgs=get_flashed_messages(with_categories=True))
         finally:
             conn.close()
 
-    return render_template('registrace.html')
+    return render_template('registrace.html', msgs=get_flashed_messages(with_categories=True))
 
 @app.route('/prihlaseni')
 def prihlaseni():
-    return render_template('prihlaseni.html') 
+    return render_template('prihlaseni.html', msgs=get_flashed_messages(with_categories=True)) 
 
 @app.route('/restaurace')
 def restaurace():
-    return render_template('restaurace.html')    
+    return render_template('restaurace.html', msgs=get_flashed_messages(with_categories=True))    
 
 @app.errorhandler(404)
 def nenalezeno(e):
-    return render_template('error404.html'), 404
+    return render_template('error404.html', msgs=get_flashed_messages(with_categories=True)), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
